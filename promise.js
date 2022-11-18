d5b7be1db44e7c3e5d31ca566772bc22b7e32980
exceptr <98112445+exceptr@users.noreply.github.com>
2022-11-18 18:53:32 +0300

    while

--- promise.js
+++ promise.js
@@ -13,32 +13,31 @@ function guessNubmer() {
     const numberToGuess = Math.floor(Math.random() * 1000);
     console.log("Загаданное число:", numberToGuess);
     (async () => {
+        while (true) {
         cmd = await r1.question("Введите число от 0 до 999: ")
         console.log(cmd);
         if (cmd === "q") {
             r1.close();
-            return
+            break
         }
         else if (isNaN(cmd) || cmd <= 0 || cmd > 999) {
             console.log("Вы ввели не число от 0 до 999");
-            guessNubmer();
         }
         else if (+cmd === numberToGuess) {
             r1.close();
             console.log("Вы угадали!");
             console.log("Общее количество попыток:", cnt1());
+            break
         }
         else if (+cmd > numberToGuess) {
             console.log("Вы не угадали! Загаданное число меньше введённого");
             console.log("Попытка №:", cnt1());
-            guessNubmer();
         }
         else if (+cmd < numberToGuess) {
             console.log("Вы не угадали! Загаданное число больше введённого");
             console.log("Попытка №:", cnt1())
-            guessNubmer();
         }
-    })()
+    }})()
 }
 
 guessNubmer();
